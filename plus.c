1da2650741cfe38249b08ea7c6490c1b11edde1d
akritygupta442261-maker <akritygupta442261@gmail.com>
2026-01-27 22:01:52 +0900

    Add files via upload

---
+++ plus.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+int n = 5;
+int mid = n / 2 + 1;
+
+
+for (int i = 1; i <= n; i++) {
+for (int j = 1; j <= n; j++) {
+
+
+printf("%c", (i == mid || j == mid) ? '*' : ' ');
+}
+printf("\n");
+}
+
+
+return 0;
+}
